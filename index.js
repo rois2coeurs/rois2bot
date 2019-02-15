7c7aa7ddb67f5fa816db67d7b451d3cf72574f46
rois2coeurs <rois2coeurs@gmail.com>
2019-02-15 16:45:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ client.on('ready', function() {
     }
 });
 
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'gare' || ch.name === 'bienvenue' || ch.name === 'aéroport');
+  if (!channel) return;
+  var welcome = new Discord.RichEmbed()
+  welcome.addField(`:bank: Bienvenue sur **${member.guild.name}** :bank:`, `${member.user} est le **${member.guild.memberCount}**éme membre du serveur !`)
+  welcome.setColor("#ffffff")
+  channel.send(welcome)
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'gare' || ch.name === 'bienvenue' || ch.name === 'aéroport');
+  if (!channel) return;
+  var welcome = new Discord.RichEmbed()
+  welcome.addField(`:no_pedestrians: ${member.displayName} a quitté **${member.guild.name}** :no_pedestrians:`, `Nous sommes maintenant **${member.guild.memberCount}** membres sur le serveur.`)
+  welcome.setColor("#ffffff")
+  channel.send(welcome)
+});
+
 client.on('message', message => {
     if(message.content === 'r!help'){
         var help = new Discord.RichEmbed()
@@ -21,7 +39,7 @@ client.on('message', message => {
         message.channel.send(help)
     }
 
-    if (message.content === 'r!botinfo') {
+    else if (message.content === 'r!botinfo') {
       var botinfo = new Discord.RichEmbed()
         .setTitle(":chart_with_upwards_trend: Informations sur le robot :chart_with_downwards_trend:",)
         .addBlankField()
@@ -31,10 +49,10 @@ client.on('message', message => {
         .addField(":desktop: Autres serveurs sur les quels le robot est connecté" , `**${client.guilds.size}** serveur`)
         .setColor("#ff5c33")
         .setFooter(`Demandé par ${message.author.username}`, message.author.displayAvatarURL)
-      message.channel.sendEmbed(botinfo)
+      message.channel.send(botinfo)
     }
 
-    if (message.content === 'r!userinfo') {
+    else if (message.content === 'r!userinfo') {
         var userCreateDate = message.author.createdAt.toString().split(" ");
         var msgauthor = message.author.id;
 
