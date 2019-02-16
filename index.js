5572fc922fe336e4eb909b2df38e52d4446f61a8
rois2coeurs <rois2coeurs@gmail.com>
2019-02-16 23:39:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// _____-----=====[Réaction role]=====-----_____
+const yourID = "234647775621414912";
+const setupCMD = "!createrolemessage"
+let initialMessage = `**Réagissez aux messages ci-dessous pour recevoir le rôle associé. Si vous souhaitez supprimer le rôle, supprimez simplement votre réaction!**`;
+const roles = ["Membre"];
+const reactions = ["✅"];
+
+if (roles.length !== reactions.length) throw "La liste des rôles et la liste des réactions ne sont pas de la même longueur!";
+
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`==================\nRéagis en dessous pour obtenir le rôle : **"${role}"** !`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+client.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+
+client.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = client.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == client.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != client.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
+});
+// _____-----=====[Fin de Réaction role]=====-----_____
+
 client.on('ready', function() {
     console.log('[INFORMATION] robot prêt')
     setInterval(intervalle, 10000, 'inter')
